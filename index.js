740509dbb4b51393f877de829d0df26748a815e0
Carly Lord <carly.lord@gmail.com>
2020-03-21 00:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var whatBeatlesPlay = []
+  for (var indx = 0; indx <= (musicians.length -1); indx++){
+    whatBeatlesPlay.push(`${musicians[indx]} plays ${instruments[indx]}`)
+  }
+  return whatBeatlesPlay
+}
+
